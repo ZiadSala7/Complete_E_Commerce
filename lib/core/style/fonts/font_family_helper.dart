b395c7b8878209b4c40534382eac85453fb2c4f2
Ziad Salah <zslah1935@gmail.com>
2024-04-24 11:37:10 +0200

    fonts creation

---
+++ lib/core/style/fonts/font_family_helper.dart
@@ -0,0 +1,5 @@
+class FontFamilyHelper {
+  FontFamilyHelper._();
+  static const String arabicFont = 'Cairo';
+  static const String englishFont = 'Poppins';
+}
---
+++ lib/core/style/fonts/font_weight_helper.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FontWEightHelper {
+  FontWEightHelper._();
+  static const boldWeight = FontWeight.w700;
+  static const mediumWeight = FontWeight.w500;
+  static const regularWeight = FontWeight.w400;
+}
